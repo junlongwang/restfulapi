1987a38c279c4649919321d74130a5c23fab4f3b
lishaoyong <lishaoyong@58ganji.com>
2016-10-23 22:30:25 +0800

    服务层按角色合并代码

--- src/main/java/com/joybike/server/api/util/UnixGps.java
+++ src/main/java/com/joybike/server/api/util/UnixGps.java
@@ -15,10 +15,6 @@ public class UnixGps {
      * @return
      */
     public static Double doLongitude(double distance) {
-//        double lngDegree = 1 * Math.asin(Math.sin((double)distance/12742)/Math.cos(distance));
-//        // 转换弧度
-//        lngDegree = lngDegree * (180/Math.PI);
-//        return lngDegree;
         double latDegrees = 180 / PI * 1 / 6372.797;
         double lngR = latDegrees / cos(distance * PI / 180);
         return lngR;
@@ -50,15 +46,6 @@ public class UnixGps {
         return latDegrees;
     }
 
-    public static void main(String args[]) {
-
-        double b = Dimension(116.355149);
-        System.out.println(b);
-
-        double a = Longitude(40.0276731);
-        System.out.println(a);
-    }
-
     /**
      * 距离转换成纬度
      *
@@ -66,11 +53,6 @@ public class UnixGps {
      * @return
      */
     public static Double doDimension(double distance) {
-//        double latDegrees = (double)distance/6371;
-//        // 转换弧度
-//        latDegrees = latDegrees * (180/Math.PI);
-//        return latDegrees;
-
         double latDegrees = 180 / PI * 1 / 6372.797;
         return latDegrees;
     }

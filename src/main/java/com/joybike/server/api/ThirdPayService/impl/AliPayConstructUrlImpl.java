27ecea29a52637fd2f7d5e64f4f25b9ea37ce9b8
longziyuan <longziyuan@aliyun.com>
2016-10-31 11:34:25 +0800

    //支付宝参数调整

--- src/main/java/com/joybike/server/api/ThirdPayService/impl/AliPayConstructUrlImpl.java
+++ src/main/java/com/joybike/server/api/ThirdPayService/impl/AliPayConstructUrlImpl.java
@@ -25,10 +25,12 @@ public class AliPayConstructUrlImpl implements AliPayConstructUrlInter{
 
     private String partner = "2088521096580226";
     private String account = "1314159";
+    private String seller_email = "wangliang@sktbj.com";
     private String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBANkzPpaA1tYpcRXEq3l4ykaCeK5AZYj07n8EfdKkQxr6uo2Lzw8g5jwgFdkO330VtoVvClRnrsjF/d6WRiao/6slBIgDrvqEbWEXtudyqLzkl2DZGnmtQpC5q7q56P5bBAMfeli0ZmYW3rjdveGf1TRDGMYYR4x87V998XNA4UgNAgMBAAECgYAEAOXOcGGFYQ4skIt4mblgw1bmH1m/xIQA41xOXai+/pAhu8n9RWX5Bb5hWdzUuWm72+gc1ixqlvuu9qYkYEkWHcjZS4TqOANCqtSCWp4hlRGVCRfHtm1wDL72Z2AF7BZIRwPnRhS9apGm1kCSCH3iSYHJCijZS3T1ooPzWJ1dAQJBAP5A1PfmGvS2WbsCHy44Ib5Gd4JP82SuKLz/IqFu9tD23x7ZLc1zvQVkKfyBr5pFw6+lJ0t2IlSiPW68sdckyXUCQQDasT7V6ZCHxbgVrOb4C7CC4/ZVxUSLTYYKckjECrVKbov+DFaPojy2IGfoJGZaWJlk0FNeOBmQFKZouXJ1dBk5AkBYUlEo5GhMxeOZ0Pzf42PlYzk0rW1RdiZ0sPRou9FFedy8LJl6m0/4RXlIXAySPNXjeC2USy9V0x4gD7B/minZAkEAwQZS1NIrvHr6iT8sOeFvcYguI/RTFLVfSxcmPMrKyyCZtalEOdDTz1j4/YArSzEKa14pR28yuOZRHvwYF61amQJAA2GH8JB4p4rGx/yw0dY3ZDGJMpjOed3MZDnJ1blrDG3akdJM8qU8H/niH5uLm5zYjAk0VyUmg/9jXJyHFfjpsw==";
     private String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZMz6WgNbWKXEVxKt5eMpGgniuQGWI9O5/BH3SpEMa+rqNi88PIOY8IBXZDt99FbaFbwpUZ67Ixf3elkYmqP+rJQSIA676hG1hF7bncqi85Jdg2Rp5rUKQuau6uej+WwQDH3pYtGZmFt643b3hn9U0QxjGGEeMfO1fffFzQOFIDQIDAQAB";
     private String ALIPAY_PAY_SERVICE = "mobile.securitypay.pay";
     private String notifyUrl = "http://api.joybike.com.cn/restful/pay/paynotify";
+    private String app_id = "2016102202289143";
 
     public RedirectParam getUrl(HashMap<String,String> paraMap) {
         RedirectParam para = new RedirectParam();
@@ -51,9 +53,10 @@ public class AliPayConstructUrlImpl implements AliPayConstructUrlInter{
                 subject=paraMap.containsKey("subject")?String.valueOf(paraMap.get("subject")):"Joybike账户充值";
                 totalFee=String.valueOf(paraMap.get("total_fee"));
                 //客户端号，用于快登
-                if(paraMap.containsKey("app_id")&&(!StringUtil.isNullOrEmpty(String.valueOf(paraMap.get("app_id"))))){
-                    requestParams.put("app_id",signature+String.valueOf(paraMap.get("app_id"))+signature);
-                }
+//                if(paraMap.containsKey("app_id")&&(!StringUtil.isNullOrEmpty(String.valueOf(paraMap.get("app_id"))))){
+//                    requestParams.put("app_id",signature+String.valueOf(paraMap.get("app_id"))+signature);
+//                }
+                requestParams.put("app_id",signature+app_id+signature);
                 //客户端来源
                 if(paraMap.containsKey("appenv")&&(!StringUtil.isNullOrEmpty(String.valueOf(paraMap.get("appenv"))))){
                     requestParams.put("appenv",signature+String.valueOf(paraMap.get("appenv"))+signature);
@@ -83,7 +86,8 @@ public class AliPayConstructUrlImpl implements AliPayConstructUrlInter{
                 requestParams.put("subject",signature+subject+signature);
                 //requestParams.put("body",signature+body+signature);
                 requestParams.put("payment_type",signature+ "1" +signature);
-                requestParams.put("seller_id",signature+account+signature);//AlipayPropertiesConfigEnum.ALIPAY_SELLER_ID.getEnumValues()
+                //requestParams.put("seller_id",signature+account+signature);//AlipayPropertiesConfigEnum.ALIPAY_SELLER_ID.getEnumValues()
+                requestParams.put("seller_email",signature+seller_email+signature);
                 requestParams.put("total_fee",signature+totalFee+signature);
                 String preSignStr = AlipayAppUtil.createLinkString(requestParams);
                 String sign = RSASignature.sign(preSignStr, privateKey, "utf-8");

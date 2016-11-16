e7b27c95f1049c7fb1c03a1b3824e8c08404a646
耿金锋 <gengjinfeng@ganji.com>
2016-11-16 16:17:52 +0800

    DEV:INIT

--- src/main/java/com/joybike/server/api/restful/PlatformRestfulApi.java
+++ src/main/java/com/joybike/server/api/restful/PlatformRestfulApi.java
@@ -39,7 +39,7 @@ public class PlatformRestfulApi {
         //如果appKey非法，返回失败
         if(!appKeys.contains(appKey))
         {
-            return  ResponseEntity.ok(new Message<Token>(false,0,"非法请求！",null));
+            return  ResponseEntity.ok(new Message<Token>(false,1001,"非法请求！",null));
         }
         String access_token = null;
         Token token = null;

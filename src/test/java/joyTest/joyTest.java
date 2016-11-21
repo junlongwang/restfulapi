a843925049fbcb70dc8a39ee9a9866e3a78f81e0
lishaoyong <lishaoyong@58ganji.com>
2016-11-21 23:36:02 +0800

    修复了如果有多条预约单的情况

--- src/test/java/joyTest/joyTest.java
+++ src/test/java/joyTest/joyTest.java
@@ -280,7 +280,7 @@ public class joyTest {
     @Test
     public void ADtEST(){
         try {
-            VehicleOrderDto dto = bicycleRestfulService.lock("107801214941", 1479666656, 116.287, 40.043);
+            VehicleOrderDto dto = bicycleRestfulService.lock("10780121494", 1479721564, 116.287, 40.043);
             System.out.println(dto);
         } catch (Exception e) {
             e.printStackTrace();
